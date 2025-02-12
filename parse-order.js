fb1859f23c40fd7aed503c05e7a4db12cf284211
b1f6c1c4 <b1f6c1c4@gmail.com>
2025-02-12 02:17:43 -0500

    gemini, parsing result

---
+++ parse-order.js
@@ -0,0 +1,180 @@
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const parse = async (client, res) => {
+  const p = {};
+  const o = { entry: p };
+
+  const doc = await client.db().collection('earnings_cleaned').findOne({
+    '_id.symbol': res._id.symbol,
+    '_id.quarter': res._id.quarter,
+  });
+  o.optimal = doc.optimal;
+
+  if (!res.order) {
+    p.error = 'no order detected';
+    return o;
+  }
+  const match = res.order.match(/^(?<side>BUY|SELL)\s+(?<position>[+-]\$?[0-9,]+\.?[0-9]*)\s+(?:of\s+)?(?<symbol>[A-Z]+);\s*(?:BUY|SELL)\s+LMT\s+(?<limit>@?\$?[0-9]+\.?[0-9]*|[+-][0-9]+\.?[0-9]*%)\s+STP\s+(?<stop>@?\$?[0-9]+\.?[0-9]*|[+-][0-9]+\.?[0-9]*%)$|^DO NOT TRADE (?<symbol>[A-Z]+)$/);
+  if (!match) {
+    p.error = 'order syntax error';
+    return o;
+  }
+  if (match.groups.symbol !== res._id.symbol) {
+    p.error = 'symbol not matching';
+    return o;
+  }
+  if (match.groups.side === undefined) {
+    p.side = 'NEITHER';
+    p.position = 0;
+    o.profit = 0;
+    o.return = 0;
+    return o;
+  }
+  if (match.groups.position.startsWith('$')) {
+    p.error = 'position not specified in dollar amount';
+    return o;
+  }
+  p.position = +match.groups.position.replaceAll(/[\$,]/g, '');
+  if (!p.position) {
+    p.error = 'invalid position';
+    return o;
+  }
+
+  p.price = match.groups.size === 'BUY'
+    ? doc.nextDayBooks[0].askL
+    : doc.nextDayBooks[0].bidH;
+  if (match.groups.side === 'BUY') {
+    if (p.position <= 0) {
+      p.error = 'position sign error';
+      return o;
+    }
+    p.side = 'BUY';
+    p.shares = Math.floor(p.position / p.price);
+    if (!p.shares) {
+      p.error = 'buying less than 1 share';
+      return o;
+    }
+  } else if (match.groups.side === 'SELL') {
+    if (p.position >= 0) {
+      p.error = 'position sign error';
+      return o;
+    }
+    p.side = 'SELL';
+    p.shares = -Math.floor(-p.position / p.price);
+    if (!p.shares) {
+      p.error = 'selling less than 1 share';
+      return o;
+    }
+  } else {
+    // unreachable
+  }
+
+  p.position = p.shares * p.price;
+  if (match.groups.limit.endsWith('%')) {
+    p.limit = +match.groups.limit.substr(0, match.groups.limit.length - 1);
+    if (p.shares > 0)
+      p.limit = p.price * (1 + p.limit / 100);
+    else
+      p.limit = p.price * (1 - p.limit / 100);
+  } else {
+    p.limit = +match.groups.limit.replace(/^@/, '');
+  }
+  if (isNaN(p.limit)) {
+    p.error = 'syntax error';
+    return o;
+  }
+  if (p.shares > 0 && p.limit < p.price) {
+    p.error = 'sell limit too low';
+    return o;
+  }
+  if (p.shares < 0 && p.limit > p.price) {
+    p.error = 'buy-to-cover limit too high';
+    return o;
+  }
+  if (match.groups.stop.endsWith('%')) {
+    p.stop = +match.groups.stop.substr(0, match.groups.stop.length - 1);
+    if (p.shares > 0)
+      p.stop = p.price * (1 + p.stop / 100);
+    else
+      p.stop = p.price * (1 - p.stop / 100);
+  } else {
+    p.stop = +match.groups.stop.replace(/^@/, '');
+  }
+  if (isNaN(p.stop)) {
+    p.error = 'syntax error';
+    return o;
+  }
+  if (p.shares > 0 && p.stop > p.price) {
+    p.error = 'sell stop too high';
+    return o;
+  }
+  if (p.shares < 0 && p.stop < p.price) {
+    p.error = 'buy-to-cover stop too low';
+    return o;
+  }
+
+  for (const v of doc.nextDayBooks) {
+    if (p.shares > 0 && v.bidH >= p.limit) {
+      o.exit = { time: v.etTimeOfDay, price: v.bidH, type: 'SOLD LMT' };
+      break;
+    }
+    if (p.shares < 0 && v.askL <= p.limit) {
+      o.exit = { time: v.etTimeOfDay, price: v.askL, type: 'BOUGHT LMT' };
+      break;
+    }
+    if (p.shares > 0 && v.askL < p.stop || p.shares < 0 && v.bidH > p.stop) {
+      const info = {
+        schema: 'bbo-1m',
+        'meta.symbol': res._id.symbol,
+        'meta.interval': '1m',
+        etDate: doc.date,
+        etTimeOfDay: { $gte: v.etTimeOfDay },
+      };
+      const book = await client.db().collection('prices_cleaned').findOne(info);
+      if (!book) {
+        p.error = 'cannot find book info';
+        p.info = info;
+        return o;
+      }
+      if (p.shares > 0)
+        o.exit = { time: v.etTimeOfDay, price: book.bid, type: 'SOLD STP' };
+      else
+        o.exit = { time: v.etTimeOfDay, price: book.ask, type: 'BOUGHT STP' };
+      break;
+    }
+  }
+  if (!o.exit) {
+    if (p.shares > 0)
+      o.exit = { time: 16 * 60, price: doc.nextDayMOC[0].bid, type: 'SOLD MOC' };
+    else
+      o.exit = { time: 16 * 60, price: doc.nextDayMOC[0].ask, type: 'BOUGHT MOC' };
+  }
+
+  o.profit = p.shares * (o.exit.price - p.price);
+  if (p.shares > 0)
+    o.return = o.exit.price / p.price - 1;
+  else
+    o.return = 1 - o.exit.price / p.price;
+  return o;
+};
+
+
+const client = new MongoClient(process.env.MONGO_URL);
+
+//             buySellPattern: '(BUY|SELL)',
+//             amountPattern: '([+-]?\\$?[0-9,]+(?:\\.[0-9]{2})?)',
+//             orderPattern: '(@[0-9]+\\.?[0-9]*|[+-]?[0-9]+%)',
+(async () => {
+  await client.connect();
+  const coll = client.db().collection('llm_outputs');
+  const res = await coll.find({
+    order: { $ne: null },
+  }).toArray();
+  console.log(`working on ${res.length} documents`);
+  await Promise.all(res.map(async (r) => coll.updateOne({ _id: r._id }, {
+    $set: await parse(client, r),
+  })));
+  console.log('finishing');
+  await client.close();
+})();
---
+++ query-gemini.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const fs = require('node:fs/promises');
+const { default: pThrottle } = require('p-throttle');
+const { MongoClient } = require('mongodb');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const client = new MongoClient(process.env.MONGO_URL);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const throttle = pThrottle({
+  limit: 1,
+  interval: 4100,
+});
+const askGemini = (model) => {
+  const llm = genAI.getGenerativeModel({ model });
+  const coll = client.db().collection('llm_outputs');
+  const reg = /^(DO NOT TRADE .*|BUY .*|SELL .*)/m;
+  const api = throttle(async (x) => {
+    console.log(`asking ${model} for ${x.length} long prompt`);
+    return llm.generateContent(x);
+  });
+  return async (fn) => {
+    const m = fn.match(/^(?<symbol>.*)_(?<quarter>.*)\.txt$/);
+    const _id = { symbol: m.groups.symbol, quarter: m.groups.quarter, model };
+    if (await coll.findOne({ _id }))
+      return;
+    const prompt = await fs.readFile('desc/' + fn, 'utf8');
+    const result = await api(prompt);
+    const text = result.response.text();
+    console.log(`got answer from ${model} length ${text.length}`);
+    const mm = text.match(reg);
+    await coll.updateOne({ _id }, {
+      $set: {
+        text,
+        order: mm ? mm[0] : null,
+      },
+    }, {
+      upsert: true,
+    });
+  };
+};
+
+(async () => {
+  await client.connect();
+  const dir = await fs.readdir('desc');
+  console.log(`working on ${dir.length} entries`);
+  await Promise.all(dir.map(askGemini('gemini-2.0-flash')));
+  await Promise.all(dir.map(askGemini('gemini-1.5-flash')));
+  await client.close();
+})();
